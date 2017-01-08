8bc1cf53297c05ddee76cd2f0bed75ec023df149
Alex Trzeciak <trebek08@gmail.com>
2017-01-08 14:29:44 -0800

    rainbow editor

--- javascript.js
+++ javascript.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 	function compile(){
+
 		var html = document.getElementById("html"); 
 		var css = document.getElementById("css"); 
 		var js = document.getElementById("js"); 
 		var code = document.getElementById("code").contentWindow.document; 
 
 		document.body.onkeyup = function(){
-			code.open(); 
-			code.writeln(html.value + "<style>"+css.value+"</style>" + "<script>" + js.value + "</script>"); 
-			code.close(); 
-		}
+			var test = document.getElementsByTagName('body')[0];
+			var num1 = Math.floor(Math.random()*255); 
+			var num2 = Math.floor(Math.random()*255); 
+			var num3 = Math.floor(Math.random()*255); 
+			var color = "rgb(" + num1 + "," + num2 +"," + num3 + ")";
 
+			test.style.backgroundColor = color;
+				code.open(); 
+				code.writeln(html.value + "<style>"+css.value+"</style>" + "<script>" + js.value + "</script>"); 
+				code.close(); 
+			}
 	}
 
 	compile(); 
